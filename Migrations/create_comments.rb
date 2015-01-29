1d57d365dd47536bce6c2f8afeb66a417802566c
Lyra-B <glykeriapeppa@gmail.com>
2015-01-29 11:50:29 +0000

    Created Migrations

---
+++ Migrations/create_comments.rb
@@ -0,0 +1,16 @@
+class CreateComments < ActiveRecord::Migration
+	
+	def up
+		create_table :comments do |t|
+			t.text :body
+			t.text :kudos
+			t.timestamps null:false
+			t.integer :post_id
+			t.integr :author_id
+		end
+	end
+
+	def down 
+		drop_table :comments
+	end
+end
